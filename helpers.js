5f1abc5e04099960e1b2dd0cacd90c65053e8199
Sunny Ripert <sun@agencecosmic.com>
2009-03-04 13:49:54 +0100

    Added Array#enqueue()

--- helpers.js
+++ helpers.js
@@ -1,3 +1,10 @@
 // Shortest way to test for IE
 IE='\v'=='v'
 
+// Moves the first element in an array to the end
+// Example: [1, 2, 3, 4].enqueue() # => [2, 3, 4, 1]
+Array.prototype.enqueue = function() {
+	this.push(this.splice(0, 1)[0]);
+	return this;
+}
+
