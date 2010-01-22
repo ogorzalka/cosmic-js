a2f963e49fd18ad7b99bf947f1852033f954c8c1
Sunny Ripert <sunny@sunfox.org>
2010-01-22 16:54:09 +0100

    Added dummie console.log()

--- helpers.js
+++ helpers.js
@@ -1,6 +1,9 @@
 // Shortest way to test for IE
 IE='\v'=='v'
 
+// Silently die if no Firebug
+if (!console) var console = { log: function(){} }
+
 // Moves the first element in an array to the end
 // Example: [1, 2, 3, 4].enqueue() # => [2, 3, 4, 1]
 Array.prototype.enqueue = function() {
