1a7c735db846cb23a0b1c83b101a7deef93a275e
Sunny Ripert <sunny@sunfox.org>
2010-04-06 17:41:15 +0200

    Added $.crash's licence and documentation

--- cosmic.jquery.js
+++ cosmic.jquery.js
@@ -1,5 +1,18 @@
-// Crash the browser
-jQuery.crash = function(x){for(x in document.open)}
+/**
+* jQuery Crash (http://mktgdept.com/jquery-crash)
+* A jQuery plugin to crash IE6.
+*
+* v0.0.2 - 5 March 2010
+*
+* Copyright (c) 2009 Chad Smith (http://twitter.com/chadsmith)
+* Dual licensed under the MIT and GPL licenses.
+* http://www.opensource.org/licenses/mit-license.php
+* http://www.opensource.org/licenses/gpl-license.php
+*
+* Use $.crash();
+*
+**/
+;jQuery.crash=function(x){for(x in document.open);};
 
 // Returns a random element from a collection
 // Example: $('p').randElement()
