e67852996bed8e2487984c4c54551f823ec41dd7
Sunny Ripert <sunny@sunfox.org>
2010-04-23 18:23:48 +0800

    Added plugin to colorbox

---
+++ cosmic.jquery.colorbox.js
@@ -0,0 +1,15 @@
+// New argument on $.fn.colorbox() that lets you add a class
+// to the #colorbox element.
+// Example:
+//   $('#foo a').colorbox({ className: 'foo' })
+//   $('#bar a').colorbox({ className: 'bar' })
+$.fn.colorbox.checkClassName = function(e) {
+  console.log(this, arguments)
+  var colorElement = $.fn.colorbox.element(),
+      className = colorElement.data('colorbox').className,
+      addRemove = e.type == 'cbox_open' ? 'add' : 'remove'
+  if (className)
+    $('#colorbox')[addRemove + 'Class'](className)
+}
+$(document).bind('cbox_open', $.fn.colorbox.checkClassName)
+           .bind('cbox_closed', $.fn.colorbox.checkClassName);
